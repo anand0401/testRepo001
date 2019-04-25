27e2490c4d0d094fa8a0693143c1786d7e816839
anand0401 <anand.behera@incture.com>
2019-04-25 10:42:58 +0000

    Project Setup

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.test.TestRepo001.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
